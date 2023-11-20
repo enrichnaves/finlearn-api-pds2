a7e65cbb5c9ce941df34f7d446e5c1fd4340f20f
enrichnaves <70281466+enrichnaves@users.noreply.github.com>
2023-11-20 15:35:13 -0300

    fix contact endpoint

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter
 from sqlalchemy.orm import configure_mappers
 from app.domains.user.adapters.orm import start_mappers as user_start_mappers
 from app.domains.network.adapters.orm import start_mappers as network_start_mappers
-from app.api.v1.endpoints import user, auth, network
+from app.domains.contact.adapters.orm import start_mappers as contact_start_mappers
+from app.api.v1.endpoints import user, auth, network, contact
 
 user_start_mappers()
 network_start_mappers()
+contact_start_mappers()
 configure_mappers()
 
 
